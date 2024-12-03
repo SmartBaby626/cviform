f3cdbe9270726a3ad093aee2a55146bd3f4ea689
Aidan White <awhit626@eq.edu.au>
2024-12-03 11:27:35 +1000

    Url fix

--- buildFile.js
+++ buildFile.js
@@ -173,7 +173,7 @@ function checkRequiredFields() {
 async function buildDocument(childNameD, dateOfBirthD) {
     try {
         // Pass parameters as query string in the URL
-        const url = `https://cviform-server.vercel.app/downloads/cviform-server/.netlify/functions/makeDoc?childName=${encodeURIComponent(childNameD)}&dateOfBirth=${encodeURIComponent(dateOfBirthD)}`;
+        const url = `https://https://cviform-server.vercel.app/Downloads/cviform-server/.netlify/functions/makeDoc?childName=${encodeURIComponent(childNameD)}&dateOfBirth=${encodeURIComponent(dateOfBirthD)}`;
 
         const response = await fetch(url, { method: "GET" });
 
